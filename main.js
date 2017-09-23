7402b81e84b52deb5c10398adfe5604cdebf0cfb
johnlyden <john.lyden86@gmail.com>
2017-09-23 17:46:32 -0500

    adding some comments about the class and the calculateHash method

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const SHA256 = require('crypto-js/sha256');
 
+/*
+* class Block
+*/
 class Block {
 	constructor(index, timestamp, data, previousHash = '') {
 		this.index = index;
@@ -10,8 +13,15 @@ class Block {
 	}
 	/*
 	* hashing function
+	* returns hashed result of string of all info 
 	*/
 	calculateHash() {
 		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+	}
+}
+
+class BlockChain {
+	constructor() {
+		this.chain = [];
 	}
 }
