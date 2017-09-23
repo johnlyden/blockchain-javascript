a3955a4cc3782e89a18316edc948faaf7fe0a8a5
johnlyden <john.lyden86@gmail.com>
2017-09-23 17:20:49 -0500

    added hashing function

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 	constructor(index, timestamp, data, previousHash = '') {
 		this.index = index;
 		this.timestamp = timestamp;
 		this.data = data;
 		this.previousHash = previousHash;
-		this.hash = '';
+		this.hash = this.calculateHash(); 
 	}
-
+	/*
+	* hashing function
+	*/
 	calculateHash() {
-
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
