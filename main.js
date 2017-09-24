33f6e88c807c82f6715e5b6d4edd9d478957d5db
johnlyden <john.lyden86@gmail.com>
2017-09-23 22:46:13 -0500

    added blockchain methods to add new block and get latest block

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const SHA256 = require('crypto-js/sha256');
 
-/*
-* class Block
-*/
-class Block {
+/** class representing a Block of the Blockchain */
+class Block{
+
+	/**
+	 * 
+	 * @param { Number } index 
+	 * @param { String } timestamp 
+	 * @param { Object } data 
+	 * @param { String } previousHash 
+	 */
 	constructor(index, timestamp, data, previousHash = '') {
 		this.index = index;
 		this.timestamp = timestamp;
@@ -11,17 +17,40 @@ class Block {
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash(); 
 	}
-	/*
-	* hashing function
-	* returns hashed result of string of all info 
-	*/
+
+	// returns a hashed string of all information of this block
 	calculateHash() {
 		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
 
+/** class representing the Blockchain */
 class BlockChain {
+
 	constructor() {
-		this.chain = [];
+		this.chain = [this.createGenesisBlock()]; 
+	}
+
+	// create the first block of the chain
+	createGenesisBlock() {
+		return new Block(0, "01/01/2017", "Genesis Block", 0);
+	}
+
+	// returns the latest block
+	getLatestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock) {
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
 	}
 }
+
+let johnCoin = new BlockChain();
+
+johnCoin.addBlock(new Block(1, "9/21/2017", {amount: 4}));
+johnCoin.addBlock(new Block(2, "9/22/2017", {amount: 10}));
+
+console.log(JSON.stringify(johnCoin, null, 4));
