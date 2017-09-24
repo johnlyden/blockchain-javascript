aad0d06569aee7cf54ff32b2edf0357d30c9535a
johnlyden <john.lyden86@gmail.com>
2017-09-23 22:59:50 -0500

    added method to check if chain is valid - added driver code to test what happens if you try to change a block

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const SHA256 = require('crypto-js/sha256');
 class Block{
 
 	/**
-	 * 
 	 * @param { Number } index 
 	 * @param { String } timestamp 
 	 * @param { Object } data 
@@ -18,7 +17,9 @@ class Block{
 		this.hash = this.calculateHash(); 
 	}
 
-	// returns a hashed string of all information of this block
+	/**
+	 * returns hashed string representing all information of the block
+	 */
 	calculateHash() {
 		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
@@ -31,26 +32,63 @@ class BlockChain {
 		this.chain = [this.createGenesisBlock()]; 
 	}
 
-	// create the first block of the chain
+	/**
+	 * crest the first block of the chain
+	 */
 	createGenesisBlock() {
 		return new Block(0, "01/01/2017", "Genesis Block", 0);
 	}
 
-	// returns the latest block
+	/**
+	 * returns the last block of the chain
+	 */
 	getLatestBlock() {
 		return this.chain[this.chain.length - 1];
 	}
 
+	/**
+	 * adds the previousHash and hash properties - then adds block to chain
+	 * @param { Block } newBlock 
+	 */
 	addBlock(newBlock) {
 		newBlock.previousHash = this.getLatestBlock().hash;
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	/**
+	 * returns true if chain is valid
+	 */
+	isChainValid() {
+		for(let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i -1];
+
+			if(currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+
+			if(currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
+/** create new instance */
 let johnCoin = new BlockChain();
-
+/** add some blocks */
 johnCoin.addBlock(new Block(1, "9/21/2017", {amount: 4}));
 johnCoin.addBlock(new Block(2, "9/22/2017", {amount: 10}));
 
-console.log(JSON.stringify(johnCoin, null, 4));
+console.log(`is blockchain valid? ${johnCoin.isChainValid()}`)
+
+/** try to f with the chain */
+johnCoin.chain[1].data = { amount: 100 };
+johnCoin.chain[1].hash = johnCoin.chain[1].calculateHash();
+
+console.log(`is blockchain valid? ${johnCoin.isChainValid()}`)
+
+
+// console.log(JSON.stringify(johnCoin, null, 4));
